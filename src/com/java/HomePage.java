416b0c19666d52e26e0aa8938f618d34c9251c96
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-04-25 15:18:38 +0800

    restructure

--- src/com/java/HomePage.java
+++ src/com/java/HomePage.java
@@ -1,21 +1,15 @@
 package com.java;
 
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
 
 public class HomePage extends JFrame
 {
@@ -27,39 +21,30 @@ public class HomePage extends JFrame
      */
     public HomePage()
     {
-        setAutoRequestFocus(false);
-        setIconImage(Toolkit.getDefaultToolkit().getImage("F:\\\u56FE\u7247\\ps\u7D20\u6750\\\u6241\u5E73\u5316\\Nokia-MixRadio.png"));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setAutoRequestFocus(true);//窗口自动最前
+        setIconImage(Toolkit.getDefaultToolkit().getImage("F:\\\u56FE\u7247\\ps\u7D20\u6750\\\u6241\u5E73\u5316\\Nokia-MixRadio.png"));//设置左上角图标
+        /*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
         setBounds(100, 100, 650, 450);
         contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		/*contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));*/
         setContentPane(contentPane);
-        contentPane.setLayout(null);
+        contentPane.setLayout(null);//设置绝对布局
 
-        JTextPane textPane = new JTextPane();
-        textPane.setBounds(104, 34, 466, 55);
-        textPane.setBackground(UIManager.getColor("Button.background"));
-        textPane.setForeground(new Color(0, 0, 0));
-        textPane.setEditable(false);
-        textPane.setFont(new Font("华文行楷", Font.PLAIN, 41));
-        textPane.setText("\u6B22\u8FCE\u4F7F\u7528\u5B66\u7C4D\u7BA1\u7406\u7CFB\u7EDF");
-        contentPane.add(textPane);
+        //欢迎语
+        JLabel label = new JLabel("欢迎使用学籍管理系统");
+        label.setFont(new Font("华文行楷", Font.PLAIN, 41));
+        label.setBounds(108, 53, 466, 55);
+        contentPane.add(label);
 
-        JLabel lblNewLabel = new JLabel("");
+        JLabel lblNewLabel = new JLabel();
         lblNewLabel.setBounds(394, 171, 196, 196);
         contentPane.add(lblNewLabel);
         lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-        lblNewLabel.setToolTipText("");
-        lblNewLabel.setIcon(new ImageIcon("F:\\\u56FE\u7247\\BayMax.png"));
+		/*lblNewLabel.setToolTipText("");*/
+        lblNewLabel.setIcon(new ImageIcon("F:\\\u56FE\u7247\\BayMax.png"));//设置标签图片
 
         JButton button = new JButton("查询学生信息");
-        button.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-            }
-        });
         button.setFont(new Font("微软雅黑", Font.PLAIN, 21));
         button.setBounds(108, 159, 229, 35);
         contentPane.add(button);
@@ -76,12 +61,6 @@ public class HomePage extends JFrame
 
         JButton button_3 = new JButton("删除学生信息");
         button_3.setFont(new Font("微软雅黑", Font.PLAIN, 21));
-        button_3.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent arg0)
-            {
-            }
-        });
         button_3.setBounds(108, 342, 229, 35);
         contentPane.add(button_3);
 
